40550b997aec8001eef6fd7afff0441ea152ace4
Shabhaizafar <99945178+Shabhaizafar@users.noreply.github.com>
2023-05-25 11:07:29 +0530

    new

---
+++ JS/02.js
@@ -0,0 +1,107 @@
+// ● String Indexing :  Collaction of Char // Array
+
+// var myStr1= "This is a Javascript1.This is a Javascript1.This is a Javascript1.";//double  quote
+// var myStr2='This is a Javascript2';//string//single quote
+// var myStr3 = `This is a Javascript3`;//backtick
+
+// console.log(myStr1);
+// console.log(myStr2);
+// console.log(myStr3);
+
+//length  1     2       3     4     5
+        //Z     A       F     A     R
+//index   0     1       2     3     4    length-1
+
+//length
+// var myStr1 = "Zafar";
+// console.log(myStr1.length);
+// console.log(myStr1[myStr1.length-1]);
+////////////////////////////////
+// ● Useful string methods
+//                     12345  
+// var myStr = "This is a Javascript Session.This is a Javascript Session.";
+// String length
+// console.log(myStr.length);
+
+
+// String slice()
+// console.log(myStr.slice(0,10));
+//index                 start , index end
+// console.log(myStr.slice(10,5));
+
+// String substr()
+// console.log(myStr.substr(0,10));
+//index                 start , number
+// console.log(myStr.substr(10,5));
+
+// String substring()
+// console.log(myStr.substring(0,10));
+//index                 start , end
+// console.log(myStr.substring(10,5));
+// console.log(myStr.substring(5,10));
+// console.log(myStr.slice(5,10));
+
+
+
+
+// var myStr = "This is a Javascript Session.This is a Javascript Session.";
+// var myStr2 = "Zafar";
+// String replace()
+// console.log(myStr);
+// console.log(myStr.replace("Javascript","JS"));
+
+// String replaceAll()
+// console.log(myStr.replaceAll("Javascript","JS"));
+
+// String toUpperCase()
+// console.log(myStr.toUpperCase());
+
+// String toLowerCase()
+// console.log(myStr.toLowerCase());
+
+
+// String concat()
+// console.log(myStr.concat(myStr2,". End of String .",myStr2));
+
+// console.log(myStr2,"Zafar");
+// console.log(myStr2+"Zafar");
+// console.log('This is :',myStr);
+// console.log(`This is :`,myStr);
+
+
+
+// String charAt()
+// console.log(myStr.charAt(0));
+
+// var Char = 'z';
+// String charCodeAt()
+// console.log(Char.charCodeAt(0));
+
+
+// String split()
+// var myStr = "This is a Javascript Session.This is a Javascript Session.";
+// console.log(myStr.split("i"));
+// console.log(myStr)
+
+// String trim()
+// String trimStart()
+// String trimEnd()
+// String padStart()
+// String padEnd()
+//////////////////////////////////////
+// ● Template Strings
+// ● Null, undefined, BigInt, typeof
+// ● Booleans and Comparison Operator
+// ● Truthy and Falsy Values
+// ● If else statement
+// ● Ternary Operator
+// ● && || operator
+// ● Nested if else
+// ● If elseif else
+// ● Switch statement
+// ● While loop
+// ● While loop examples
+// ● For loop
+// ● For loop examples
+// ● Break and continue keyword
+// ● Do while loop
