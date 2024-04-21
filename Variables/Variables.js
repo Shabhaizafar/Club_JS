38f83799fb1eb0678dfc1311c1364814e1567b8d
Shabhaizafar <zafarShabhai03@gmail.com>
2024-04-21 15:51:01 +0530

    JS

---
+++ Variables/Variables.js
@@ -0,0 +1,47 @@
+// int float string char double 
+
+// int a = 5;
+// float b = 5.5;
+// char c = 'c';
+
+
+// using var keyword 
+a = 5;
+var b = 4.4;
+var c = 'c';
+var fname = "Zafar";
+var Arr = [1,2,3,4,5];
+
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(fname);
+console.log(Arr);
+
+//typeof Operator : 
+
+//Number
+console.log(typeof(a));
+console.log(typeof b);
+
+//String
+console.log(typeof c);
+console.log(typeof fname);
+
+// Boolean 
+console.log(true);
+console.log(typeof true);
+
+//undefined 
+console.log(undefined);
+console.log(typeof undefined);
+
+//Object
+console.log( typeof Arr);
+console.log(typeof {fname : "Zafar"});
+console.log(typeof null);
+
+
+// rule of Variable Name  declaration 
