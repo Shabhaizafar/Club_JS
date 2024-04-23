5e80fb63c93e3ae34f22ad138e003d12509c2f11
Shabhaizafar <zafarShabhai03@gmail.com>
2024-04-23 20:58:22 +0530

    Variables,let,var,const

---
+++ Variables/Variables2.js
@@ -0,0 +1,42 @@
+// int n1 = 12;
+// char ch = 'a';
+// float f1 = 3.3;
+
+
+
+//use : variable  declaration
+// var 
+// let 
+// const 
+
+// var n1 = true;
+// let n2 = false;
+// const n3 = true;
+
+// console.log(n1);
+// console.log(n2);
+// console.log(n3);
+
+
+// var n1 = 12;
+// console.log("Value of N1 :",n1);
+// n1 =100;
+// console.log("Value of N1 :",n1);
+// var n1 = 1000;
+// console.log("Value of N1 :",n1);
+
+
+
+// let n2 = 13;
+// console.log("Value of N2 :",n2);
+// n2 = 200;
+// console.log("Value of N2 :",n2);
+// // let n2 = 2000;
+
+
+
+// const n3 = 14;
+// console.log("Value of N3 :",n3);
+// n3 = 400;
+// console.log("Value of N3 :",n3);
+// const n3 = 20;
