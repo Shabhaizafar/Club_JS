ad6d61c106dff42c87c48753e7052b211d6e66fc
Shabhaizafar <zafarShabhai03@gmail.com>
2024-05-26 15:05:03 +0530

    Backup

---
+++ Backup All/All.js
@@ -0,0 +1,72 @@
+// ✅ Hello world program.
+//Main Screen 
+// document.write("HTML");
+
+//console Screen
+// console.log("Hello2");
+//////////////////////////////////////
+// How to Use HTML Element in  JS : 
+
+// document.write("<h1>Hello</h1>");
+
+// console.log("Hello\n\tEveryone");
+// ////////////////////////////////
+// chrome with Console :
+// Print
+
+// console.time("Time");
+// console.log("sdfg");
+// console.error("This is an Error");
+// console.warn("Warning");
+// console.log([1,2,3,4,5,6]);
+
+
+// console.table([1,2,3,4,5,6]);
+// console.timeEnd("Time");
+
+
+////////////////////////////////
+// Datatype  : Number,string,boolean,undefined,object(null,Array,object,set,map.....),function,bigInt
+// ✅ Declare variable using var.
+// var n1 = 12;
+// var s1 ='a';
+// var b1 = true;
+// var a1 = null;
+// var u1;
+
+// console.log(n1,typeof(n1));
+// console.log(s1,typeof s1);
+// console.log(b1,typeof b1);
+// console.log(a1,typeof a1);
+// console.log(u1,typeof u1);
+
+
+////////////////////////////
+
+
+// /////////////////////////////////////
+// ✅ More about variable.
+//  start with only char or _ or $
+// don't start with number or special char
+// don't use space or desh in a variable name 
+// must try to use camelCase
+// firstname    firstName
+// /////////////////////////////////////////
+// ✅ Let.
+// let n1 = 13;
+// // console.log(n1);
+// ✅ Const.
+// const n1 = 13;
+// console.log(n1);
+/////////////////////////
+
+// ------------------------------
+// ✅ Null, undefined, BigInt, typeof.
+// ✅ Booleans and Comparison Operator.
+// ✅ Truthy and Falsy Values :
+
+
+///////////////////////////////////////
+// ✅ String Indexing.
+// ✅ Useful string methods.
+// ✅ Template Strings.
