b42b5e783b45f377e87cf8ddce89238bb40893dd
Shabhaizafar <zafarshabhai03.com>
2024-07-02 20:56:07 +0530

    Array

--- Array/Arr4.js
+++ Array/Arr4.js
@@ -1,8 +1,15 @@
 // - For loop. (Basic) 
 var Arr = ["Zafar","Z","A","F","A","R"];
+
+// index   , value , condition important
 // for (let i = 0; i < Arr.length; i++) {
 //     console.log("Index:",i,"Value is : ",Arr[i]);
 // }
+
+
+
+
+// index   , value , condition important
 // - While loop in array.
 // var i = 0;
 // while (i<Arr.length) {
@@ -11,8 +18,59 @@ var Arr = ["Zafar","Z","A","F","A","R"];
 // }
 
 
-// - For of loop.
-for (const value of Arr) {
-    console.log(value);
-}
+// - For of loop.   (value)   (index -->  X)
+// for (const value of Arr) {
+//     console.log(value);
+// }
+// for (const z of Arr) {
+//     console.log(z);
+// }
+
+
 // - For in loop.
+// index ,value  (Condition -->X)
+// for (const i in Arr) {
+//    console.log(i,Arr[i]);
+// }
+
+// for (const key in Arr) {
+//    console.log(key,Arr[key]);
+// }
+
+
+// Extra 
+// for (let iterator of Arr) {
+//     console.log(iterator);
+// }
+// console.log(iterator);
+// for (var i = 0; i < Arr.length; i++) {
+//     console.log(i);
+// }
+// console.log(i);
+
+// var   : global scope
+// let  :   block scope
+// const : block scope
+
+
+
+
+//for-each Loop : 
+//  value  , index ,Array
+// Arr.forEach((value,i,a)=> {
+//     console.log(value,i,a);
+// });
+
+
+///////////////////////////////////
+
+// 1. Write a for loop that iterates from 1 to 10 and prints each number.
+// 2. Use a while loop to print numbers from 10 to 1 (descending order).
+// 3. Create an array of numbers and use a for loop to iterate over each element and print them.
+// 4. Write a for...in loop to iterate over the properties of an object and print them.
+// 5. Use a for...of loop to iterate over elements of an array and print each element.
+// 6. Implement a nested for loop to print a multiplication table up to 10x10.
+// 7. Write a while loop that simulates a countdown from 10 to 0, printing each number with a delay of one second.
+// 8. Use a for loop to find and print all even numbers between 1 to 20.
+// 9. Create an array of objects with name and age properties, then use a for...of loop to print only the names.
+// 10. Write a do...while loop that asks the user for a number repeatedly until they enter a number greater than 100.
