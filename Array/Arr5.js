ff81e0b8d608c63694b969dec6d422e35123944e
Shabhaizafar <zafarshabhai03.com>
2024-07-07 15:46:41 +0530

    Arr6

---
+++ Array/Arr5.js
@@ -0,0 +1,12 @@
+// 7. Write a while loop that simulates a countdown from 10 to 0, printing each number with a delay of one second.
+// var i = 10;
+// var v = setInterval(() => {
+//     console.log(i--);
+//     if(i==-1)
+//     {
+//         clearInterval(v);
+//     }
+// }, 1000);
+  
+
+// 9. Create an array of objects with name and age properties, then use a for...of loop to print only the names.
---
+++ Array/Arr6.js
@@ -0,0 +1,70 @@
+// - use const for creating arrays.
+
+// const Arr = [1,2,3];
+// console.log("Before Change : ",Arr);
+
+// Arr.push("Zafar");
+// console.log("After Changed : ",Arr);
+/////////////////////////////////////////////////////
+
+// - Array destructuring.
+
+// var ArrayofIntNumber = [1,2,3,4,5,6,7,8];
+// console.log(Arr);
+
+// Arr[0]   =  1
+
+// Arr[3]   =  4
+// console.log(ArrayofIntNumber[0]);
+
+// var v1 
+// var v2
+// var v3
+
+
+// var [v1,,v,...newArr] = ArrayofIntNumber;
+
+// console.log(v1);
+// console.log(v);
+// console.log(v1+v);
+
+// console.log(newArr);
+
+
+///////////////////////////////////////
+// 1. **Using `const` with Arrays:**
+//    - Create an array of numbers using `const` and calculate the sum of its elements.
+//    - Define a `const` array of strings representing days of the week and log each day to the console.
+
+// 2. **Array Destructuring:**
+//    - Write a program that swaps the values of two variables using array destructuring.
+//    - Implement the first and last elements of an array using array destructuring.
+
+// 3. **Combined Practice:**
+//    - Implement a program that accepts an array of arrays (matrix), uses array destructuring to find and log the diagonal elements.
+////////////////////////////////////////////////////////////////
+
+
+//    - Implement a program that uses `const` arrays to store student names and their respective grades, and then display each student's name and grade.
+// const Arr = [];
+// var n = +prompt("How many number of Students ?: ");
+
+// for (let i = 0; i < n; i++) {
+//     var Arr2 = [];
+//     Arr2.push(prompt("Enter Student Name : "),+prompt("ENter Marks : "));
+//     Arr.push(Arr2);
+// }
+
+// console.log(...Arr);
+
+
+//    - Create a program that accepts an array of numbers, uses array destructuring to extract the minimum and maximum values, and then calculates their difference.
+// var Arr = [12,14,15,12,1,13,14,18];
+
+// console.log(Arr);
+
+// var [minimum,,,,,,,maximum]  = Arr.sort();
+// console.log(minimum);
+// console.log(maximum);
+
+
