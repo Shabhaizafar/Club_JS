43d787c0d742ab614c83d0af00e8780b606058a7
Shabhaizafar <zafarshabhai03.com>
2024-07-10 20:50:12 +0530

    Object

---
+++ Object/IntroObj.js
@@ -0,0 +1,82 @@
+// Objects in JavaScript :
+// {} ==> object
+// key and value pairs   == property
+// method --> later
+
+
+// var obj = {};
+// console.log(obj);
+// console.log(typeof obj);
+
+var obj = {
+    //key    value       property
+    fname : "Raj",
+    lname : "Shah"
+}
+
+
+// // 1) How to print full object 
+console.log(obj);
+
+///////////////////////////////////////////////////////////////////////
+// //2) how to Access property value
+
+// //i)  using dot notation
+// console.log(obj.fname);
+// //ii) using bracket notation
+// console.log(obj["lname"]);
+///////////////////////////////////////////////////////////////////////
+//3) How to Modify Property value
+
+// //i)  using dot notation
+// obj.fname = "Ajay";
+// console.log(obj)
+// // //ii) using bracket notation
+// obj['lname'] = "Patel";
+// console.log(obj);
+///////////////////////////////////////////////////////////////////////
+//4) How to add new Property 
+
+//i)  using dot notation
+// obj.Gender = "Male";
+// console.log(obj)
+// //ii) using bracket notation
+// obj[`Age`] = 12;
+// console.log(obj);
+///////////////////////////////////////////////////////////////////////
+//5) How to Delete Property 
+
+//i)  using dot notation
+// delete obj.fname;
+// console.log(obj);
+// //ii) using bracket notation
+// delete obj['lname'];
+// console.log(obj);
+
+// delete obj;  // not possible
+// console.log(obj);
+
+
+
+
+
+////////////////////////////////////////////////////////
+
+// let car = {
+//     brand: "Toyota",
+//     model: "Camry",
+//     year: 2022,
+//     color: "Silver"
+//   };
+
+
+
+// Print full object: How would you log the entire car object to the console?
+
+// Access property value: Using dot notation, how do you access the brand of the car?
+
+// Modify property value: If you wanted to update the year of the car to 2023, how would you do it?
+
+// Add new property: What steps would you take to add a mileage property with a value of 15000 to the car object?
+
+// Delete property: How can you remove the color property from the car object?
