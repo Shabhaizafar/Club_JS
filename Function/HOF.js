e70bc8da66cb23b28646aa2b76fdc7308f86d0b9
Shabhaizafar <zafarshabhai03.com>
2024-08-03 14:30:11 +0530

    Function

--- Function/HOF.js
+++ Function/HOF.js
@@ -1,47 +1,74 @@
-function main(){
-    var choice = prompt("1) type 'num' for Number\n2) type 'str' for string.\n\nEnter your Choice :");
-    switch (choice) {
-        case 'num': Numbers();
-            break;
-        case 'str': Strings();
-            break;
-        default: console.log("Please Enter Valid Choice !!!!");
-            break;
-    }
-}
-// main();
+// function main(){
+//     var choice = prompt("1) type 'num' for Number\n2) type 'str' for string.\n\nEnter your Choice :");
+//     switch (choice) {
+//         case 'num': Numbers();
+//             break;
+//         case 'str': Strings();
+//             break;
+//         default: console.log("Please Enter Valid Choice !!!!");
+//             break;
+//     }
+// }
+// // main();
 
 
 
 
-function Numbers(){
-    var n1 = +prompt("Enter the Vlaue of n1 :");
-    var n2 = +prompt("Enter the Vlaue of n2 :");
-    var choice = prompt("1) '+' for Addition\n2) '-' for Subtraction.\n\nEnter your Choice : ");
-    switch (choice) {
-        case '+': Addition(n1,n2);
-            break;
-        case '-': Subtraction(n1,n2);
-            break;
-        default: console.log("Please Enter Valid Choice !!!!");
-            break;
-    }
-}
+// function Numbers(){
+//     var n1 = +prompt("Enter the Vlaue of n1 :");
+//     var n2 = +prompt("Enter the Vlaue of n2 :");
+//     var choice = prompt("1) '+' for Addition\n2) '-' for Subtraction.\n\nEnter your Choice : ");
+//     switch (choice) {
+//         case '+': Addition(n1,n2);
+//             break;
+//         case '-': Subtraction(n1,n2);
+//             break;
+//         default: console.log("Please Enter Valid Choice !!!!");
+//             break;
+//     }
+// }
 
-function Strings(){
-    var s1 = +prompt("Enter the Vlaue of s1 :");
-    var s2 = +prompt("Enter the Vlaue of s2 :");
+// function Strings(){
+//     var s1 = +prompt("Enter the Vlaue of s1 :");
+//     var s2 = +prompt("Enter the Vlaue of s2 :");
  
-    // 1) concatination 
-    // 2) concatination  with Lowercase
-    // 3) concatination  with Uppercase
-    // 4) concatination  and find total length
+//     // 1) concatination 
+//     // 2) concatination  with Lowercase
+//     // 3) concatination  with Uppercase
+//     // 4) concatination  and find total length
+// }
+
+// function Addition(a,b){
+//     console.Additionlog(`Addition of  ${a} and ${b} : ${a+b}.`);
+// }
+
+// function Subtraction(s1,s2){
+//     console.log(`Subtraction of ${s1} and ${s2} : ${s1-s2}.`);
+// }
+
+// 2. Implement a function `filterArray` that takes an array `arr` and a callback function `callback`. The callback function should return `true` or `false` based on some condition. `filterArray` should return a new array containing only the elements for which the callback function returns `true`.
+
+
+var Arr = [12,1,10,30,34,54,21,9,5,7];
+function filterArray(arr,callback){
+    var newArr = [];
+    for (const iterator of arr) {
+        if(callback(iterator))
+        {
+            newArr.push(iterator);
+        }
+    }
+    return newArr;
 }
 
-function Addition(a,b){
-    console.Additionlog(`Addition of  ${a} and ${b} : ${a+b}.`);
-}
+console.log(filterArray(Arr,All));
 
-function Subtraction(s1,s2){
-    console.log(`Subtraction of ${s1} and ${s2} : ${s1-s2}.`);
+function All(n){
+    if(n>20)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
 }
--- Function/callback.js
+++ Function/callback.js
@@ -15,7 +15,9 @@
 /////////////////////////////////////////////
 // ### Callback Functions:
 // 1. Create a function `multiply` that takes two parameters `x` and `y` and a callback function `callback`. Inside `multiply`, call the callback function with the result of `x * y`.
-// 2. Implement a function `filterArray` that takes an array `arr` and a callback function `callback`. The callback function should return `true` or `false` based on some condition. `filterArray` should return a new array containing only the elements for which the callback function returns `true`.
 // 3. Write a function `delayedGreet` that takes two parameters: `name` and `callback`. Inside `delayedGreet`, use `setTimeout` to call the callback function after 2 seconds with `"Hello, <name>!"`.
 // 4. Explain the concept of a "higher-order function" and provide an example of a higher-order function that accepts a callback function as an argument.
 // 5. Create a function `forEach` that takes an array `arr` and a callback function `callback`. `forEach` should call the callback function for each element in the array.
+
+////////////////////////////////////////////////////
+// 2. Implement a function `filterArray` that takes an array `arr` and a callback function `callback`. The callback function should return `true` or `false` based on some condition. `filterArray` should return a new array containing only the elements for which the callback function returns `true`.
